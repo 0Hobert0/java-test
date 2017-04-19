97578c3a73909c95f5b6891216e6b5d8714a5466
hobert <xiaoqu0722@hotmail.com>
2017-04-19 23:25:58 +0800

    添加文件

---
+++ test.java
@@ -0,0 +1,9 @@
+//这是一个测试main函数参数的程序，需在dos窗口下测试
+public class test 
+{
+	public static void main(String[] x)
+	{
+		System.out.println("This is a java programmer!");
+//		System.out.println("arg is"+x[0]+" "+x[1]+" "+x[2]);
+	}
+}
